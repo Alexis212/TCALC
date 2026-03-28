2f76cd0b9dbe95f0f2827d91006caaa73ceb1485
Alexis212 <serafiosalcedo@gmail.com>
2026-03-28 14:28:29 -0600

    Backspace behaviour and fixed input; Add include std::cout

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,50 +1,96 @@
+#include <exception>
 #include <string>
+#include <vector>
 #include <iostream>
 #include <cctype>
 #include "helpers.hpp"
 
+using std::cout;
+using std::cin;
+
 
 int main(int argc, char *argv[])
 {
-    std::string buffer;
+    bool is_quit = false;
+    bool is_dot_in_buffer = false;
+
     char char_buffer;
+    std::string buffer;
+    std::vector<double> stack;
+
     enableRawMode();
 
     while (true)
     {
         buffer = "";
-        std::cout << " > ";
+        cout << " > ";
 
-        while (std::cin.get(char_buffer))
+        while (cin.get(char_buffer))
         {
+            // Behavior of BACKSPACE
             if (char_buffer == '\x7f')
             {
                 if (!buffer.empty())
                 {
+                    if (buffer.back() == '.')
+                        is_dot_in_buffer = false;
+
                     buffer.pop_back();
-                    std::cout << "\b \b";
+                    cout << "\b \b";
                 }
 
                 continue;
             }
 
-            if (char_buffer == '\n' || char_buffer == ' ')
+            else if (char_buffer == 'q')
             {
-                std::cout << "\n";
+                is_quit = true;
+                cout << "\n";
                 break;
             }
 
-            if (std::isprint(char_buffer))
+            // Send data with RET or SPACE
+            else if (char_buffer == '\n' or char_buffer == ' ')
             {
-                std::cout << char_buffer;
+                cout << "\n";
+                break;
+            }
+
+            // Only send '.' or a digit to buffer
+            else if (char_buffer == '.' and not is_dot_in_buffer)
+            {
+                cout << char_buffer;
+                buffer.push_back(char_buffer);
+                is_dot_in_buffer = true;
+            }
+
+            else if (std::isdigit(char_buffer))
+            {
+                cout << char_buffer;
                 buffer.push_back(char_buffer);
             }
         }
 
-        std::cout << buffer << "\n";
-
-        if (buffer == "exit")
+        if (is_quit)
             break;
+
+        is_dot_in_buffer = false;
+
+        try
+        {
+            stack.push_back(std::stod(buffer));
+
+            if (buffer.empty())
+                cout << "." << "\n";
+
+            else
+                cout << stack.back() << "\n";
+        }
+
+        catch (std::exception &e)
+        {
+            cout << "FIXME: Buffer read more than it need" << "\n";
+        }
     }
 
     disableRawMode();
